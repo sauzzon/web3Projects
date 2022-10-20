0662acd0538434ffd9bd93ab22a449313f735c4b
sauzzon <saujan.tiwari@gmail.com>
2022-10-20 18:19:13 +0545

    contract and test scripts written

--- BuyMeACoffee/README.md
+++ BuyMeACoffee/README.md
@@ -1,13 +1,3 @@
 # Sample Hardhat Project
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+BuyMeACoffee deployed to Goerli: 0x2854b11e86d74982e8921Db725c8501bDa049553
--- BuyMeACoffee/hardhat.config.js
+++ BuyMeACoffee/hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
